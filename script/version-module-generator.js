3f92d9443511bf5876d185c22a9db385e8c1862f
tokghwt <tokghwt@gmail.com>
2023-08-31 16:33:02 +0900

    chore: Create version module generator - 2

--- script/version-module-generator.js
+++ script/version-module-generator.js
@@ -1,3 +1,9 @@
 const { writeFileSync } = require('node:fs');
 
-writeFileSync(process.argv[2], `module.exports = '${process.env.npm_package_version}';`);
+if (process.argv[2] === undefined) {
+  console.error('Usage: node version-module-generator.js OUTPUT');
+  process.exitCode = 1;
+  return;
+}
+
+writeFileSync(process.argv[2], `module.exports = '${process.env.npm_package_version}';\n`);
